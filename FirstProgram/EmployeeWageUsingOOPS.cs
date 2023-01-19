e0abfa1d1b82911fa1248815acc71e96742fcd2d
devashree0900 <85434516+devashree0900@users.noreply.github.com>
2023-01-19 20:05:43 +0530

    Add files via upload

---
+++ FirstProgram/EmployeeWageUsingOOPS.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+
+    public class EmployeeWageUsingOOPS
+    {
+        const int isPartTime = 1;
+        const int isFullTime = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmployeeWageUsingOOPS(string company, int empRatePerHour, int numOfWorkingDays, int maxHrsPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour= empRatePerHour;
+            this.numOfWorkingDays= numOfWorkingDays;
+            this.maxHoursPerMonth= maxHrsPerMonth;
+        }
+
+
+        /*public void computeEmpWageUC7()
+        {
+            
+            int empRatePerHour = 20;
+            int numOfWorkingDays = 20;
+            int maxHrsInMonth = 100;
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+            while (totalEmpHrs < maxHrsInMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+
+
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+
+        }
+
+        public void computeEmpWageUC8(string company, int empRatePerHour, int numOfWorkingDays , int maxHrsInMonth)
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+            while (totalEmpHrs < maxHrsInMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage + " for the company: " + company);
+
+
+        }*/
+
+        public void computeEmpWageUC9()
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+
+            while (totalEmpHrs < maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+
+
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage + " for the company: " + company);
+
+        }
+
+        public void UC10()
+        {
+            //Remaining
+        }
+
+    }
+}
---
+++ FirstProgram/LineComparisonUsingOOPS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    public class Line
+    {
+        public double x1, x2, y1, y2;
+        public Line(double a1, double b1, double a2, double b2)
+        {
+            this.x1 = a1;
+            this.x2 = b1;   
+            this.y1 = b2;   
+            this.y2 = a2;
+        }
+    }
+    public class LineComparisonUsingOOPS
+    {
+
+        public double getLength(Line line)
+        {
+            return Math.Sqrt(Math.Pow(line.x1-line.x2,2)+Math.Pow(line.y1-line.y2,2));   
+        }
+
+        public void compare(Line line1,Line line2)
+        {
+            if(getLength(line1) == getLength(line2))
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+            else if(getLength(line1) > getLength(line2))
+            {
+                Console.WriteLine("First line has a larger length");
+            }
+            else
+            {
+                Console.WriteLine("Second line has a larger length");
+            }
+        }
+    }
+}
